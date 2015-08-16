e3d967167408cf64b1ad352a4de4ffc9cbbbfa26
HyungSuk Ryu <soulawaker@gmail.com>
2015-08-16 20:20:29 +0900

    update HelloJava

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,7 @@ public class HelloJava {
 	public static void main (String [] args) {
 		System.out.println("Java Hello World");
 		System.out.println("Hello!");
+		System.out.println("Third Hello!");
 	}
 
 }
